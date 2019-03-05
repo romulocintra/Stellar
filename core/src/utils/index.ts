5f6b6e6e414e25cc88f00bdfdbf71be8b22dcf97
William M. Riley <mikeriley20@gmail.com>
2019-03-05 15:03:16 -0600

    Initial commit for monorepo

---
+++ core/src/utils/index.ts
@@ -0,0 +1,19 @@
+export { colors } from '../global/colors';
+export { properties } from 'css-custom-properties';
+export { isHexColor } from 'validator'
+
+export function format(first: string, middle: string, last: string): string {
+  return (
+    (first || '') +
+    (middle ? ` ${middle}` : '') +
+    (last ? ` ${last}` : '')
+  );
+}
+
+export function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(' ');
+}
